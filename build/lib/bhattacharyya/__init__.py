ed9b79c58ee18be66a185591bf6e295d9d3fee36
dave-heslop74 <dave.heslop74@gmail.com>
2019-07-08 14:46:58 +1000

    updated package and notebook before paper submission

--- build/lib/bhattacharyya/__init__.py
+++ build/lib/bhattacharyya/__init__.py
@@ -36,9 +36,6 @@ def calculator(IA,DA,KA,RA,IB,DB,KB,RB):
     B = Bayes(muA,KA*RA,muB,KB*RB)
     output2 = widgets.HTML(value='<h4>Bayes error = {0:.3f}</h4>'.format(B))
     
-    #if (RA==1) & (RB==1):
-    #    Rtitle = widgets.HTML(value='<h3>Comparison of Fisher distributions:</h3>')
-    #else:
     Rtitle = widgets.HTML(value='<h3>Similarity of paleomagnetic poles:</h3>')
 
     results=widgets.VBox((Rtitle,output1,output2))
@@ -51,17 +48,17 @@ def open_console(*args):
     
     spacer = widgets.HTML(value='<font color="white">This is some text!</font>')
 
-    Atitle = widgets.HTML(value='<h4>Pole A</h4>')
-    IA=widgets.BoundedFloatText(value=0.0,min=-90.0,max=90.0,step=0.01,description='PLat$_A$ [-90$^\circ$: +90$^\circ$]:',style=style,layout=layout)
-    DA=widgets.BoundedFloatText(value=0.0,min=0.0,max=360.0,step=0.01,description='Plon$_A$ [0$^\circ$: 360$^\circ$]:',style=style,layout=layout)
-    KA=widgets.BoundedFloatText(value=0.01,min=0.0,max=100000,step=0.01,description='$\kappa_A$ [>0]:',style=style,layout=layout)
-    RA=widgets.BoundedFloatText(value=1,min=1,max=100000,step=0.01,description='$R_A$ [$\geq$1]:',style=style,layout=layout)
+    Atitle = widgets.HTML(value='<h4>Pole p</h4>')
+    IA=widgets.BoundedFloatText(value=0.0,min=-90.0,max=90.0,step=0.01,description='PLat$_p$ [-90$^\circ$: +90$^\circ$]:',style=style,layout=layout)
+    DA=widgets.BoundedFloatText(value=0.0,min=0.0,max=360.0,step=0.01,description='Plon$_p$ [0$^\circ$: 360$^\circ$]:',style=style,layout=layout)
+    KA=widgets.BoundedFloatText(value=0.01,min=0.0,max=100000,step=0.01,description='$\kappa_p$ [>0]:',style=style,layout=layout)
+    RA=widgets.BoundedFloatText(value=1,min=1,max=100000,step=0.01,description='$R_p$ [$\geq$1]:',style=style,layout=layout)
 
-    Btitle = widgets.HTML(value='<h4>Pole B</h4>')
-    IB=widgets.BoundedFloatText(value=0.0,min=-90.0,max=90.0,step=0.01,description='Plat$_B$ [-90$^\circ$: +90$^\circ$]:',style=style,layout=layout)
-    DB=widgets.BoundedFloatText(value=0.0,min=0.0,max=360.0,step=0.01,description='Plon$_B$ [0$^\circ$: 360$^\circ$]:',style=style,layout=layout)
-    KB=widgets.BoundedFloatText(value=0.01,min=0.0,max=100000,step=0.01,description='$\kappa_B$ [>0]:',style=style,layout=layout)
-    RB=widgets.BoundedFloatText(value=1,min=1.0,max=100000,step=0.01,description='$R_B$ [$\geq$1]:',style=style,layout=layout)
+    Btitle = widgets.HTML(value='<h4>Pole q</h4>')
+    IB=widgets.BoundedFloatText(value=0.0,min=-90.0,max=90.0,step=0.01,description='Plat$_q$ [-90$^\circ$: +90$^\circ$]:',style=style,layout=layout)
+    DB=widgets.BoundedFloatText(value=0.0,min=0.0,max=360.0,step=0.01,description='Plon$_q$ [0$^\circ$: 360$^\circ$]:',style=style,layout=layout)
+    KB=widgets.BoundedFloatText(value=0.01,min=0.0,max=100000,step=0.01,description='$\kappa_q$ [>0]:',style=style,layout=layout)
+    RB=widgets.BoundedFloatText(value=1,min=1.0,max=100000,step=0.01,description='$R_q$ [$\geq$1]:',style=style,layout=layout)
     
     uA = widgets.VBox((Atitle,IA, DA, KA, RA),layout=Layout(overflow_y='initial',height='180px'))
     uB = widgets.VBox((Btitle,IB, DB, KB, RB),layout=Layout(overflow_y='initial',height='180px'))
